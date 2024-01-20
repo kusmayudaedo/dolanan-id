006defadc06c744092e8e52da3b7c44a477c2216
kusmayudaedo <kusmayuda.edo@gmail.com>
2024-01-20 11:24:31 +0700

    Displaying platforms icon

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
 	game: Game;
@@ -10,6 +11,9 @@ const GameCard = ({ game }: Props) => {
 		<Card rounded={10} overflow='hidden'>
 			<Image src={game.background_image} />
 			<CardBody>
+				<PlatformIconList
+					platforms={game.parent_platforms.map((p) => p.platform)}
+				/>
 				<Heading fontSize='2xl'>{game.name}</Heading>
 			</CardBody>
 		</Card>
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,55 @@
+import {
+	FaWindows,
+	FaPlaystation,
+	FaXbox,
+	FaApple,
+	FaLinux,
+	FaAndroid,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { HStack, Icon, Text } from '@chakra-ui/react';
+import { Platform } from '../hooks/useGames';
+import { IconType } from 'react-icons';
+
+interface Props {
+	platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+	const iconMap: { [key: string]: IconType } = {
+		pc: FaWindows,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		nintendo: SiNintendo,
+		mac: FaApple,
+		linux: FaLinux,
+		ios: MdPhoneIphone,
+		android: FaAndroid,
+		web: BsGlobe,
+	};
+	const limitPlatform = 5;
+	return (
+		<HStack marginY={1}>
+			{platforms.length > limitPlatform ? (
+				<HStack>
+					{platforms.slice(0, limitPlatform).map((platform) => (
+						<Icon
+							as={iconMap[platform.slug]}
+							fontWeight='bold'
+							boxSize='16px'
+						/>
+					))}
+					<Text fontWeight='bold'>+{platforms.length - limitPlatform}</Text>
+				</HStack>
+			) : (
+				platforms.map((platform) => (
+					<Icon as={iconMap[platform.slug]} fontWeight='bold' boxSize='16px' />
+				))
+			)}
+		</HStack>
+	);
+};
+
+export default PlatformIconList;
