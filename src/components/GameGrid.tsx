b18ca7f079961b758428bc3ce0f9e224cf95d6c7
kusmayudaedo <kusmayuda.edo@gmail.com>
2024-01-20 13:03:28 +0700

    Create a genrec data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCarsSkeleton from './GameCarsSkeleton';
 import GameCardContainer from './GameCardContainer';
 
 const GameGrid = () => {
-	const { games, error, isLoading } = useGames();
+	const { data, error, isLoading } = useGames();
 	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 	return (
 		<>
@@ -21,7 +21,7 @@ const GameGrid = () => {
 							<GameCarsSkeleton key={skeleton} />
 						</GameCardContainer>
 					))}
-				{games.map((game) => (
+				{data.map((game) => (
 					<GameCardContainer>
 						<GameCard key={game.id} game={game} />
 					</GameCardContainer>
