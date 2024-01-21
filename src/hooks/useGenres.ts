6b0cf29c186b7f3fd086e18e9790f6d8ac3db89e
kusmayudaedo <kusmayuda.edo@gmail.com>
2024-01-21 16:56:29 +0700

    Refactor : simplify stale time calculation using ms

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,6 @@
 import genres from '../data/genres';
 import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
 import APIClient from '../services/api-client';
 
 export interface Genre {
@@ -14,7 +15,7 @@ const useGenres = () =>
 	useQuery({
 		queryKey: ['genres'],
 		queryFn: apiClient.getAll,
-		staleTime: 24 * 60 * 60 * 1000, // 24h
+		staleTime: ms('24h'),
 		initialData: genres,
 	});
 
