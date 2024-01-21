07237560db6bd21799a9b7f8077a2c60c5dbfc20
kusmayudaedo <kusmayuda.edo@gmail.com>
2024-01-21 10:30:22 +0700

    Add dynamic game heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from './hooks/useGenres';
 import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './hooks/useGames';
 import SortSelector from './components/SortSelector';
+import GameHeading from './components/GameHeading';
 
 export interface GameQuery {
 	genre: Genre | null;
@@ -40,6 +41,7 @@ function App() {
 				</GridItem>
 			</Show>
 			<GridItem area='main' paddingX={5} paddingY='5px'>
+				<GameHeading gameQuery={gameQuery} />
 				<HStack mb={5}>
 					<SortSelector
 						selectedOrder={gameQuery.sortOrder}
