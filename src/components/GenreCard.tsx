f0567ba9019d54ef0a22a63fa3898b4c6ee65582
kusmayudaedo <kusmayuda.edo@gmail.com>
2024-01-28 12:26:12 +0700

    FIx : vercel not found when refresh

--- src/components/GenreCard.tsx
+++ src/components/GenreCard.tsx
@@ -1,4 +1,5 @@
 import {
+	Box,
 	Button,
 	Card,
 	CardBody,
@@ -23,61 +24,71 @@ const GenreCard = ({ genre }: Props) => {
 	const navigate = useNavigate();
 	const topThreeGame = genre.games?.slice(0, 3);
 	return (
-		<Card
-			bg={`rgba(0, 0, 0, 0.1) url(${genre.image_background})`}
-			backgroundSize='cover'
-			backgroundPosition='center'
-			objectFit='fill'
-			height='250px'
+		<Box
+			_hover={{
+				transform: 'scale(1.1)',
+				borderRadius: '40',
+				transition: 'transform .15s ease-in-out;',
+				padding: '10px',
+				overflow: 'hidden',
+			}}
 		>
-			<CardBody backgroundImage='linear-gradient(rgba(255,255,255, 0.05), rgba(0, 0, 0, 1) )'>
-				<Stack spacing={4}>
-					<Flex justifyContent='center'>
-						<Button
-							variant='ghost'
-							whiteSpace='normal'
-							onClick={() => {
-								setSelectedGenreId(genre.id);
-								navigate('/');
-							}}
-						>
-							<Heading>{genre.name}</Heading>
-						</Button>
-					</Flex>
-
-					<Flex
-						justifyContent='space-between'
-						marginY={3}
-						paddingBottom={2}
-						borderBottom='1px solid rgba(255, 2555,255, 0.3)'
-					>
-						<Text fontSize='xl' fontWeight='bold'>
-							Popular items
-						</Text>
-						<Flex alignItems='center' gap={1}>
-							<Text fontSize='xl' fontWeight='bold'>
-								{genre.games_count.toLocaleString()}
-							</Text>
-							<MdGamepad />
+			<Card
+				bg={`rgba(0, 0, 0, 0.1) url(${genre.image_background})`}
+				backgroundSize='cover'
+				backgroundPosition='center'
+				objectFit='fill'
+				height='250px'
+			>
+				<CardBody backgroundImage='linear-gradient(rgba(255,255,255, 0.05), rgba(0, 0, 0, 1) )'>
+					<Stack spacing={4}>
+						<Flex justifyContent='center'>
+							<Button
+								variant='ghost'
+								whiteSpace='normal'
+								onClick={() => {
+									setSelectedGenreId(genre.id);
+									navigate('/');
+								}}
+							>
+								<Heading>{genre.name}</Heading>
+							</Button>
 						</Flex>
-					</Flex>
-				</Stack>
 
-				<Stack spacing={1}>
-					{topThreeGame?.map((game) => (
-						<Flex key={game.id} justifyContent='space-between'>
-							<Link to={`/game/${game.slug}`}>
-								<Text textDecor='underline'>{game.name}</Text>
-							</Link>
+						<Flex
+							justifyContent='space-between'
+							marginY={3}
+							paddingBottom={2}
+							borderBottom='1px solid rgba(255, 2555,255, 0.3)'
+						>
+							<Text fontSize='xl' fontWeight='bold'>
+								Popular items
+							</Text>
 							<Flex alignItems='center' gap={1}>
-								<Text>{game.added.toLocaleString()}</Text>
-								<IoPersonOutline />
+								<Text fontSize='xl' fontWeight='bold'>
+									{genre.games_count.toLocaleString()}
+								</Text>
+								<MdGamepad />
 							</Flex>
 						</Flex>
-					))}
-				</Stack>
-			</CardBody>
-		</Card>
+					</Stack>
+
+					<Stack spacing={1}>
+						{topThreeGame?.map((game) => (
+							<Flex key={game.id} justifyContent='space-between'>
+								<Link to={`/game/${game.slug}`}>
+									<Text textDecor='underline'>{game.name}</Text>
+								</Link>
+								<Flex alignItems='center' gap={1}>
+									<Text>{game.added.toLocaleString()}</Text>
+									<IoPersonOutline />
+								</Flex>
+							</Flex>
+						))}
+					</Stack>
+				</CardBody>
+			</Card>
+		</Box>
 	);
 };
 
